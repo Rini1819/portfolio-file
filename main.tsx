020e5c818550f204ddd57c6b0db2f5a429ac4da4
Rini swing <66106467+Rini1819@users.noreply.github.com>
2026-06-03 13:13:13 +0530

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,7 +1,7 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
-import { EditModeProvider } from './components/EditModeContext.tsx';
+import { EditModeProvider } from './EditModeContext.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
